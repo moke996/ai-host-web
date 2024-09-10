fb6f98e6728b26d0dba7952182b2ab6167e5f6b8
moke <hxmoke.9698@gmail.com>
2024-09-10 18:05:52 +0800

    接口调整

--- src/views/home.vue
+++ src/views/home.vue
@@ -13,7 +13,7 @@
                             </el-select>
                         </div>
                         <div  style="margin-top: 20px; margin-left: 20px;">
-                            <el-button type="primary" plain @click="dialogTableVisible = true">History</el-button>
+                            <el-button type="primary" plain @click="getHistoryList">History</el-button>
                         </div>
                         <div style="margin-top: 20px;margin-left: 20px;">
                             <el-button type="primary" plain @click="dialogFormVisible = true">Save</el-button>
@@ -22,25 +22,50 @@
                             <el-button :disabled="startTag !== 0" type="success" @click="start">Start</el-button>
                         </div>
                         <div style="margin-top: 20px;margin-left: 20px;">
-                            <el-button type="danger"  @click="reset" >Reset</el-button>
+                            <el-button type="danger" @click="reset" >Reset</el-button>
                         </div>
                     </div>
                     <div >
                         <div style=" width: 430px; margin-top: 20px;margin-left:20px; ">
-                        <el-input  type="textarea"  :rows="10" placeholder="User Profile" v-model="male"></el-input>
+                        <el-input  type="textarea"  :rows="13" placeholder="User Profile" v-model="male"></el-input>
                         </div>
                         <div style=" width: 430px; margin-top: 10px; margin-left:20px">
-                            <el-input type="textarea"  :rows="10" placeholder="User Profile" v-model="female"></el-input>
+                            <el-input type="textarea"  :rows="13" placeholder="User Profile" v-model="female"></el-input>
                         </div>
                     </div>
                 </div>
             </el-aside>
             <el-container>
-                <el-main>Main</el-main>
+                <el-main width = "800">
+                    <div>
+                        <!-- 聊天对话框 -->
+                        <div class="chat-container">
+                            <div
+                                v-for="(message, index) in messages"
+                                :key="index"
+                                :class="['chat-bubble', message.sender === 'user' ? 'user-bubble' : 'server-bubble']"
+                            >
+                            <pre>{{ message.text }}</pre> <!-- 使用 <pre> 保留换行符 -->
+                            </div>
+                        </div>
+                        <div class="input-area">
+                            <el-input
+                            v-model="userInput"
+                            type="textarea"
+                            :rows="4"
+                            placeholder="Type your message..."
+                            class="input-box"
+                            clearable
+                            ></el-input>
+                            <!-- 发送按钮 -->
+                            <el-button type="success"  class="send-button" @click="sendMessage">Send</el-button>
+                        </div>     
+                    </div>
+                </el-main>
             </el-container>
         </el-container>
 
-   
+
         <!-- Save的对话框 -->
         <el-dialog title="保存测试结果" v-model="dialogFormVisible">
             <el-form :model="saveForm">
@@ -51,7 +76,7 @@
                     <el-input v-model="saveForm.info" type="textarea"  :rows="3"  autocomplete="off"></el-input>
                 </el-form-item>
             </el-form>
-            <div slot="footer" class="dialog-footer">
+            <div style="text-align: right;">
                 <el-button @click="dialogFormVisible = false">取 消</el-button>
                 <el-button type="primary" @click="saveRecord" >确 定</el-button>
             </div>
@@ -60,9 +85,8 @@
        <!-- 历史记录对话框 -->
        <el-dialog title="历史记录" v-model="dialogTableVisible">
             <el-table :data="gridData" max-height="250">
-                <el-table-column property="date" label="日期" width="150"></el-table-column>
-                <el-table-column property="name" label="名称" width="120"></el-table-column>
-                <el-table-column property="address" label="描述" width = "300"></el-table-column>
+                <el-table-column property="name" label="名称" width="150"></el-table-column>
+                <el-table-column property="description" label="描述" width = "350"></el-table-column>
                 <el-table-column label="操作" width="150">
                     <template #default="scope">
                         <el-button @click="getHistory(scope.row)" type="primary" plain size="small">查看</el-button>
@@ -70,7 +94,7 @@
                     </template>
                 </el-table-column>
             </el-table>
-        </el-dialog>
+       </el-dialog>
 
 
   </template>
@@ -78,6 +102,7 @@
   <script>
   import axios from 'axios';
   import { ref } from 'vue';
+  import { ElAlert } from 'element-plus';
   
   export default {
     name: 'Home',
@@ -94,13 +119,14 @@
                 info:''
             },
             formLabelWidth: '120px',
-            gridData: [{
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
-            }],
+            gridData: [],
             startTag: 0,
-            BASE_URL: "http://localhost:10005"
+            BASE_URL: "http://localhost:10005",
+            userInput: '',
+            messages:[],
+            nextStep: '',
+            isDelete: 0
+
         }
     },
     methods: {
@@ -113,71 +139,139 @@
                 console.error('请求失败：', error);
             }
         },
-        start() {
+        async  start() {
             if (this.startTag === 0 ){
                 const now = new Date();
                 this.startTag = now.getTime();
             }
             try {
                 const response = await axios.post(this.BASE_URL+`/api/v1/host/start`,{
-                    
+                    "version": this.version,
+                    "tag": this.startTag,
+                    "male": this.male,
+                    "female": this.female
                 });
-                console.log('请求成功：', response.data.data);
-                this.versions = response.data.data
+                this.messages.push({ text: response.data.data.ai_answer, sender: 'server' });
+                this.nextStep = response.data.data.next_step
+                this.Run()
+
             } catch (error) {
                 console.error('请求失败：', error);
             }
         },
-        
 
-
-
-
-
-        saveRecord() {
-            this.$alert('这是一段内容', '标题名称', {
+        reset() {
+            this.$alert('此操作将会删除当前Profile, 并清除会话历史！', '请确认', {
             confirmButtonText: '确定',
             callback: action => {
-                this.$message({
-                type: 'info',
-                message: `action: ${ action }`
-                });
+                this.male = '',
+                this.female = ''
+                this.startTag = 0
+                this.userInput = ''
+                this.messages = []
+                this.messages.push( { text: 'Welcome to the chat!', sender: 'server' });
             }
             });
         },
-        deleteHistory(row){
+        async sendMessage() {
+            if (!this.userInput) return;
+            // 添加用户输入到消息列表
+            this.messages.push({ text: this.userInput, sender: 'user' });
+            this.Run()
+            // 清空用户输入
+            this.userInput = '';
+        },
+
+        async saveRecord() {
+            try {
+                const response = await axios.post(this.BASE_URL+`/api/v1/host/save`,{
+                    "version": this.version,
+                    "title": this.saveForm.title,
+                    "into": this.saveForm.info,
+                    "tag": this.startTag,
+                    "message": this.message
+                });
+                this.messages.push({ text: response.data.data.ai_answer, sender: 'server' });
+                this.nextStep = response.data.data.next_step
+                this.Run()
+
+            } catch (error) {
+                console.error('请求失败：', error);
+            }
+        },
+
+        async Run() {
+            try {
+            // 发起 HTTP POST 请求
+            const response = await axios.post(this.BASE_URL+`/api/v1/host/run`,{
+                    "version": this.version,
+                    "tag": this.startTag,
+                    "content": this.userInput,
+                    "next": this.nextStep
+                });
+
+            // 将服务器响应添加到消息列表
+            this.messages.push({ text: response.data.data.ai_answer, sender: 'server' });
+            this.nextStep = response.data.data.next_step
+            
+            for (let i = 0; i < 100; i++) {
+                if (this.nextStep !== "user_answer") {
+                    this.Run()
+                }else{
+                    break
+                }
+            }
+            } catch (error) {
+                console.error('Error making POST request:', error);
+                this.messages.push({ text: 'Error: Unable to fetch response from server', sender: 'server' });
+            }
+        },
+
+        async deleteHistory(row){
             this.$alert('确认删除该条测试记录？', '请确认', {
             confirmButtonText: '确定',
             callback: action => {
-                this.$message({
-                type: 'info',
-                message: `action: ${ action }`
-                });
+                  this.isDelete = 1  
             }
             });
+            if (this.isDelete == 1){
+                try {
+                    const response = await axios.post(this.BASE_URL+`/api/v1/host/history/`+row.id);
+                    this.$message({
+                        type: 'success',
+                        message: '删除成功！'
+                    });
+                    this.isDelete = 0
+                } catch (error) {
+                    console.error('请求失败：', error);
+                }
+            }
         },
-        getHistory(row){
-            console.log(row)
+        async getHistory(row){
+            try {
+                    const response = await axios.get(this.BASE_URL+`/api/v1/host/history/`+row.id);
+                     const data = response.data.data
+                     this.male = data.male 
+                     this.female = data.female
+                     this.version = data.version
+                     this.messages = data.messages
+                } catch (error) {
+                    console.error('请求失败：', error);
+                }
         },
-        saveRecodeDialog() {
-            console.log("111111111")
-            this.dialogFormVisible = true
-            console.log(this.dialogFormVisible)
+        async getHistoryList(row){
+            this.dialogTableVisible = true
+            try {
+                    const response = await axios.get(this.BASE_URL+`/api/v1/host/histories`);
+                    this.gridData = response.data.data
+                } catch (error) {
+                    console.error('请求失败：', error);
+                }
         },
         recordHistory() {
             console.log("")
         },
-        reset() {
-            this.$alert('您是否确认删除当前profile,并清除会话历史！', '请确认', {
-            confirmButtonText: '确定',
-            callback: action => {
-                this.$message({
-                type: 'info',
-                message: `action: ${ action }`
-                });
-            }
-            });
-        },
+
         handleClose(done) {
         this.$confirm('确认关闭？')
           .then(_ => {
@@ -188,35 +282,106 @@
     },
     mounted() {
     this.getVersion(); // 页面初始化时调用 fetchInitialData 方法
-  }
-    // setup() {
-    //   const responseData = ref(null);
-  
-    //   const sendPostRequest = async () => {
-    //     try {
-    //       const response = await axios.post('https://dev.apiteamn.com', {
-    //         // 请求体数据
-    //         key1: 'value1',
-    //         key2: 'value2'
-    //       });
-  
-    //       // 请求成功后的响应数据
-    //       responseData.value = response.data;
-    //     } catch (error) {
-    //       console.error('Error making POST request:', error);
-    //       responseData.value = error.message;
-    //     }
-    //   };
-  
-    //   return {
-    //     sendPostRequest,
-    //     responseData
-    //   };
-    // }
+    this.messages.push( { text: 'Welcome to the chat!', sender: 'server' });
+
+  },
+//   setup() {
+//     const apiUrl = 'https://dev.apiteamn.com'; // URL 变量
+//     const userInput = ref('');
+//     const messages = ref([
+//       // 初始聊天记录
+//       { text: 'Welcome to the chat!', sender: 'server' }
+//     ]);
+//     const sendMessage = async () => {
+//       if (!userInput.value) return; // 如果没有输入内容，则不发送
+
+//       // 添加用户输入到消息列表
+//       messages.value.push({ text: userInput.value, sender: 'user' });
+
+//       try {
+//         // 发起 HTTP POST 请求
+//         // const response = await axios.post(apiUrl, {
+//         //   message: userInput.value // 发送用户输入到服务器
+//         // });
+
+//         // 将服务器响应添加到消息列表
+//         // messages.value.push({ text: response.data.reply, sender: 'server' });
+//          messages.value.push({ text: "这是模拟回复", sender: 'server' });
+//       } catch (error) {
+//         console.error('Error making POST request:', error);
+//         messages.value.push({ text: 'Error: Unable to fetch response from server', sender: 'server' });
+//       }
+
+//       // 清空用户输入
+//       userInput.value = '';
+//     };
+//     const reset = async () => {
+//             alert('您是否确认删除当前profile,并清除会话历史！', '请确认', {
+//             confirmButtonText: '确定',
+//             callback: action => {
+//                 this.male = "",
+//                 this.female = ""
+//                 this.startTag = 0;
+//             }
+//             });
+//     };
+//     return {
+//       userInput,
+//       messages,
+//       sendMessage,
+//       reset
+//     };
+//   }
   };
   </script>
   
   <style>
-  /* 添加样式 */
+.chat-container {
+  max-width: 950px;
+  margin-top: 8px ;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  padding: 10px;
+  height: 500px;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.chat-bubble {
+  padding: 10px;
+  border-radius: 15px;
+  margin-bottom: 5px;
+  max-width: 70%;
+  word-wrap: break-word;
+  white-space: pre-wrap; /* 保留空白和换行 */
+}
+
+.user-bubble {
+  background-color: #d4f7d4; /* 绿色气泡 */
+  align-self: flex-start;
+  text-align: left;
+}
+
+.server-bubble {
+  background-color: #c6c2c2; /* 白色气泡 */
+  align-self: flex-start;
+  text-align: left;
+}
+
+.input-area {
+  display: flex;
+  align-items: flex-start;
+}
+
+.input-box {
+  margin-top: 10px;
+  flex: 1;
+  margin-right: 10px;
+}
+
+.send-button {
+  align-self: flex-end;
+}
   </style>
   
